f29ff56b4a8e9992fb1f0b735d7db18945aa8752
关明 <lennonguan@foxmail.com>
2023-09-27 17:35:32 +0800

    math库添加gcd/lcm/reduction函数

--- builtin_libs/math.go
+++ builtin_libs/math.go
@@ -27,12 +27,38 @@ func libMath(*Context) ValueObject {
 		}
 		return nil
 	}), nil)
+	r.SetMember("gcd", NewNativeFunction("gcd", func(c *Context, _ Value, args []Value) Value {
+		return NewInt(mathGCD(c, "gcd", args))
+	}), nil)
+	r.SetMember("lcm", NewNativeFunction("lcm", func(c *Context, _ Value, args []Value) Value {
+		gcd := mathGCD(c, "lcm", args)
+		r := int64(1)
+		for i, a := range args {
+			r *= a.(ValueInt).Value()
+			if i == 0 {
+				r /= gcd
+			}
+		}
+		return NewInt(r)
+	}), nil)
+	r.SetMember("reduction", NewNativeFunction("reduction", func(c *Context, _ Value, args []Value) Value {
+		gcd := mathGCD(c, "reduction", args)
+		rv := NewArray(len(args))
+		for _, a := range args {
+			rv.PushBack(NewInt(a.(ValueInt).Value() / gcd))
+		}
+		return rv
+	}), nil)
 	for name, fnv := range mathPortFuncs {
 		switch fn := fnv.(type) {
 		case func(float64) float64:
 			r.SetMember(name, mathPortFuncF_F(name, fn), nil)
+		case func(float64) (float64, float64):
+			r.SetMember(name, mathPortFuncF_FF(name, fn), nil)
 		case func(float64, float64) float64:
 			r.SetMember(name, mathPortFuncFF_F(name, fn), nil)
+		case func(float64, float64, float64) float64:
+			r.SetMember(name, mathPortFuncFFF_F(name, fn), nil)
 		}
 	}
 	for name, v := range mathPortConsts {
@@ -74,6 +100,17 @@ func mathPortFuncFFF_F(name string, fn func(float64, float64, float64) float64) 
 			ArgRuleRequired("c", TypeFloat, &c),
 		)
 		return NewFloat(fn(a.Value(), b.Value(), c.Value()))
+	})
+}
+
+func mathPortFuncF_FF(name string, fn func(float64) (float64, float64)) ValueCallable {
+	return NewNativeFunction(name, func(ctx *Context, _ Value, args []Value) Value {
+		var a ValueFloat
+		EnsureFuncParams(ctx, "math."+name, args,
+			ArgRuleRequired("a", TypeFloat, &a),
+		)
+		r1, r2 := fn(a.Value())
+		return NewArrayByValues(NewFloat(r1), NewFloat(r2))
 	})
 }
 
@@ -149,4 +186,42 @@ var mathPortFuncs = map[string]any{
 var mathPortConsts = map[string]any{
 	"PI": math.Pi,
 	"E":  math.E,
+}
+
+func mathGCD2(a, b int64) int64 {
+	for b > 0 {
+		t := a % b
+		a = b
+		b = t
+	}
+	return a
+}
+
+func mathMustGetPositiveInt(c *Context, a Value) int64 {
+	if ai, is := a.(ValueInt); !is {
+		c.RaiseRuntimeError("argument must be an integer")
+		return -1
+	} else if vi := ai.Value(); vi <= 0 {
+		c.RaiseRuntimeError("argument must be larger than 0")
+		return -1
+	} else {
+		return vi
+	}
+}
+
+func mathGCD(c *Context, f string, args []Value) int64 {
+	n := len(args)
+	if n < 2 {
+		c.RaiseRuntimeError("math.%s: requires at lease 2 arguments", f)
+		return -1
+	}
+	a := mathMustGetPositiveInt(c, args[0])
+	b := mathMustGetPositiveInt(c, args[1])
+	r := mathGCD2(a, b)
+	for i := 2; i < n; i++ {
+		a = r
+		b = mathMustGetPositiveInt(c, args[i])
+		r = mathGCD2(a, b)
+	}
+	return r
 }
