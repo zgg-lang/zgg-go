2a0178e9caf01c42a47de55424ca452d686e40e2
关明 <i@guanming.me>
2023-11-23 20:56:58 +0800

    websocket repl支持内置及指定文件的index.html

--- cmd/ws.go
+++ cmd/ws.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "embed"
 	"flag"
 	"fmt"
 	"net/http"
@@ -31,10 +32,36 @@ func log(level logLevel, msg string, args ...interface{}) {
 	fmt.Fprintf(os.Stderr, f, args...)
 }
 
+func checkAuthByScript(authScript string, w http.ResponseWriter, r *http.Request, event string) bool {
+	if authScript != "" {
+		if bs, err := os.ReadFile(authScript); err != nil {
+			log(ERROR, "read authScript %s fail: %+v", authScript, err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return false
+		} else if authHandler, err := zgg.CompileCode(string(bs)); err != nil {
+			log(ERROR, "compile authScript %s fail: %+v", authScript, err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return false
+		} else if r, err := zgg.RunCode(authHandler, zgg.Var{"event", zgg.Val{event}},
+			zgg.Var{"request", r}, zgg.Var{"responseWriter", w}); err != nil {
+			log(ERROR, "execute auth script error: %+v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return false
+		} else if pass, is := r["pass"].(bool); !is || !pass {
+			return false
+		}
+	}
+	return true
+}
+
+//go:embed wsindex.html
+var wsIndex string
+
 func runWebsocket(isDebug bool, args []string) {
-	var listen, path, authScript string
+	var listen, path, authScript, indexPath string
 	fs := flag.NewFlagSet("zgg ws", flag.ExitOnError)
 	fs.StringVar(&authScript, "auth", "", "指定鉴权处理脚本路径，留空为不鉴权")
+	fs.StringVar(&indexPath, "index", "", "指定首页HTML文件路径，留空为内置首页")
 	fs.Parse(args)
 	addr := fs.Arg(0)
 	if p := strings.Index(addr, "/"); p >= 0 {
@@ -47,27 +74,23 @@ func runWebsocket(isDebug bool, args []string) {
 	upgrader := websocket.Upgrader{
 		CheckOrigin: func(r *http.Request) bool { return true },
 	}
-	http.HandleFunc(path+"/session", func(w http.ResponseWriter, r *http.Request) {
-		clientAddr := r.RemoteAddr
-		if authScript != "" {
-			if bs, err := os.ReadFile(authScript); err != nil {
-				log(ERROR, "read authScript %s fail: %+v", authScript, err)
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			} else if authHandler, err := zgg.CompileCode(string(bs)); err != nil {
-				log(ERROR, "compile authScript %s fail: %+v", authScript, err)
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			} else if r, err := zgg.RunCode(authHandler, zgg.Var{"event", zgg.Val{"connect"}},
-				zgg.Var{"request", r}, zgg.Var{"responseWriter", w},
-				zgg.Var{"ip", zgg.Val{clientAddr}}); err != nil {
-				log(ERROR, "execute auth script error: %+v", err)
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			} else if pass, is := r["pass"].(bool); !is || !pass {
-				return
-			}
+	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		if !checkAuthByScript(authScript, w, r, "index") {
+			log(INFO, "index blocked by authscript")
+			return
 		}
+		if indexPath != "" {
+			http.ServeFile(w, r, indexPath)
+		} else {
+			http.ServeContent(w, r, "index.html", time.Time{}, strings.NewReader(wsIndex))
+		}
+	})
+	http.HandleFunc(path+"/session", func(w http.ResponseWriter, r *http.Request) {
+		if !checkAuthByScript(authScript, w, r, "connect") {
+			log(INFO, "ws blocked by authscript")
+			return
+		}
+		clientAddr := r.RemoteAddr
 		conn, err := upgrader.Upgrade(w, r, w.Header())
 		if err != nil {
 			log(ERROR, "Upgrade %s error: %v", clientAddr, err)
