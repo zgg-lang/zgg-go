09446c7f14d0add2fd1d41f649cd594d4d47cd65
关明 <lennonguan@foxmail.com>
2023-11-23 17:16:54 +0800

    dev

--- cmd/ws.go
+++ cmd/ws.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"os"
@@ -10,6 +11,7 @@ import (
 	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/zgg-lang/zgg-go"
 	"github.com/zgg-lang/zgg-go/repl"
 	"github.com/zgg-lang/zgg-go/repl/ws_repl"
 )
@@ -29,8 +31,12 @@ func log(level logLevel, msg string, args ...interface{}) {
 	fmt.Fprintf(os.Stderr, f, args...)
 }
 
-func runWebsocket(isDebug bool, addr string) {
-	var listen, path string
+func runWebsocket(isDebug bool, args []string) {
+	var listen, path, authScript string
+	fs := flag.NewFlagSet("zgg ws", flag.ExitOnError)
+	fs.StringVar(&authScript, "auth", "", "指定鉴权处理脚本路径，留空为不鉴权")
+	fs.Parse(args)
+	addr := fs.Arg(0)
 	if p := strings.Index(addr, "/"); p >= 0 {
 		listen = addr[0:p]
 		path = addr[p:]
@@ -41,8 +47,27 @@ func runWebsocket(isDebug bool, addr string) {
 	upgrader := websocket.Upgrader{
 		CheckOrigin: func(r *http.Request) bool { return true },
 	}
-	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc(path+"/session", func(w http.ResponseWriter, r *http.Request) {
 		clientAddr := r.RemoteAddr
+		if authScript != "" {
+			if bs, err := os.ReadFile(authScript); err != nil {
+				log(ERROR, "read authScript %s fail: %+v", authScript, err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			} else if authHandler, err := zgg.CompileCode(string(bs)); err != nil {
+				log(ERROR, "compile authScript %s fail: %+v", authScript, err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			} else if r, err := zgg.RunCode(authHandler, zgg.Var{"event", zgg.Val{"connect"}},
+				zgg.Var{"request", r}, zgg.Var{"responseWriter", w},
+				zgg.Var{"ip", zgg.Val{clientAddr}}); err != nil {
+				log(ERROR, "execute auth script error: %+v", err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			} else if pass, is := r["pass"].(bool); !is || !pass {
+				return
+			}
+		}
 		conn, err := upgrader.Upgrade(w, r, w.Header())
 		if err != nil {
 			log(ERROR, "Upgrade %s error: %v", clientAddr, err)
