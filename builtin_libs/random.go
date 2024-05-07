f4e2c2efbb1708ea368d3003744d8961431cb4ce
关明 <i@guanming.me>
2024-05-07 10:16:35 +0800

    random库支持直接调用，等同于调用@random.random

--- builtin_libs/random.go
+++ builtin_libs/random.go
@@ -11,28 +11,11 @@ func libRandom(*Context) ValueObject {
 	//DOC random() 		返回[0, 1)范围内的一个随机浮点数
 	//DOC random(end)	返回[0, end)范围内的一个随机整数，当end<=0或者end非整数时抛出异常
 	//DOC random(a, b)	返回[a, b)范围内的一个随机整数，当a或者b非整数，或者a>=b时抛出异常
-	lib.SetMember("random", NewNativeFunction("random", func(c *Context, this Value, args []Value) Value {
-		switch len(args) {
-		case 0:
-			return NewFloat(rand.Float64())
-		case 1:
-			n := c.MustInt(args[0], "random:end")
-			if n <= 0 {
-				c.RaiseRuntimeError("random(end): end must > 0")
-			}
-			return NewInt(rand.Int63n(n))
-		case 2:
-			m := c.MustInt(args[0], "random:began")
-			n := c.MustInt(args[1], "random:end")
-			if n <= m {
-				c.RaiseRuntimeError("random(begin, end): end must > begin")
-			}
-			return NewInt(rand.Int63n(n-m) + m)
-		default:
-			c.RaiseRuntimeError("random: requires 0/1/2 argument(s)")
-		}
-		return nil
-	}), nil)
+	lib.SetMember("random", NewNativeFunction("random", randomRandomFunc), nil)
+	//DOC __call__() 		返回[0, 1)范围内的一个随机浮点数
+	//DOC __call__(end)		返回[0, end)范围内的一个随机整数，当end<=0或者end非整数时抛出异常
+	//DOC __call__(a, b)	返回[a, b)范围内的一个随机整数，当a或者b非整数，或者a>=b时抛出异常
+	lib.SetMember("__call__", NewNativeFunction("__call__", randomRandomFunc), nil)
 	//DOC choice(array) 	返回array内的一个随机元素，当array非数组或者数组为空时抛出异常
 	lib.SetMember("choice", NewNativeFunction("choice", func(c *Context, this Value, args []Value) Value {
 		var choices ValueArray
@@ -64,4 +47,27 @@ func libRandom(*Context) ValueObject {
 		return arr
 	}), nil)
 	return lib
+}
+
+func randomRandomFunc(c *Context, this Value, args []Value) Value {
+	switch len(args) {
+	case 0:
+		return NewFloat(rand.Float64())
+	case 1:
+		n := c.MustInt(args[0], "random:end")
+		if n <= 0 {
+			c.RaiseRuntimeError("random(end): end must > 0")
+		}
+		return NewInt(rand.Int63n(n))
+	case 2:
+		m := c.MustInt(args[0], "random:began")
+		n := c.MustInt(args[1], "random:end")
+		if n <= m {
+			c.RaiseRuntimeError("random(begin, end): end must > begin")
+		}
+		return NewInt(rand.Int63n(n-m) + m)
+	default:
+		c.RaiseRuntimeError("random: requires 0/1/2 argument(s)")
+	}
+	return nil
 }
